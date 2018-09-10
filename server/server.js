cc41d18448780c71ff2573dcded91982acb39874
Emmanuel Casas <emmanuelcasas@dhcp-10-154-188-114.vpn.oracle.com>
2018-09-10 14:00:20 -0500

    Add connect and disconnect event handler

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
-
+const socketIO = require('socket.io');
 const express = require('express');
-
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+  console.log('New user connected');
 
-app.listen(port, ()=>{
+  socket.on('disconnect', ()=>{
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, ()=>{
   console.log('Server started at port', port);
 });
