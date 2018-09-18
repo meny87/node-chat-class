dc3de26f4ae243ecd0c12ee3cb15bc311aa0582a
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-09-18 16:13:55 -0500

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -97,7 +97,6 @@ jQuery('#message-form').on('submit', function(e) {
   var messageTextBox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'Client',
     text: messageTextBox.val()
   }, function() {
     messageTextBox.val('');
